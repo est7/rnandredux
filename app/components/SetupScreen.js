a66cbeb28fd627c497d8665ecd9172b99934ca80
lihang <lihang@droi.com>
2017-07-14 13:38:06 +0800

    5%

--- app/components/SetupScreen.js
+++ app/components/SetupScreen.js
@@ -1,9 +1,21 @@
 /**
  * Created by Administrator on 2017/7/4.
  */
+
+'use strict'
+
 import { Button, Image, StyleSheet }from 'react-native'
 import React, { Component } from 'react'
+
 export default class SetupScreen extends React.Component {
+
+  constructor (props) {
+    super(props)
+
+  }
+
+
+
   static navigationOptions = {
     tabBarLabel: 'Setup',
     tabBarIcon: ({tintColor}) => (
@@ -19,6 +31,7 @@ export default class SetupScreen extends React.Component {
         title="Go back to home tab"
         onPress={() => goBack()}
       />
+
     )
   }
 }
